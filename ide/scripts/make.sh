2bcc7c2b5c08bbe7df5bfb2cfdf9b446c10e4451
zsawyer <zsawyer@sourceforge.net>
2013-04-09 16:21:30 +0200

    ! fixed errors in build scripts + added compiled libs (32 and 64 bit) for OSX (10.8) + added compiled libs (32 and 64 bit) for linux (with "g++ (Debian 4.3.2-1.1) 4.3.2")

--- ide/scripts/make.sh
+++ ide/scripts/make.sh
@@ -3,15 +3,15 @@
 # UNTESTED!!
 
 # 32 bit
-rm -R build
+#rm -R build
 mkdir build
 
-g++  -o build/libLinkAPI.so -shared \
+g++  -o build/libLinkAPI.so -shared -fPIC \
      -Wl,-soname,build/libLinkAPI.so  \
      -lrt \
-     -fvisibility=hidden
-     -fvisibility-inlines-hidden
-     ../../LinkAPI.cpp \
+     -fvisibility=hidden \
+     -fvisibility-inlines-hidden \
+     ../../src/LinkAPI.cpp \
         -lstdc++
 
 # 64 bit
@@ -19,16 +19,16 @@ g++  -o build/libLinkAPI.so -shared \
 g++  -o build/libLinkAPI64.so -m64 -shared -fPIC \
      -Wl,-soname,build/libLinkAPI64.so  \
      -lrt \
-     -fvisibility=hidden
-     -fvisibility-inlines-hidden
-     ../../LinkAPI.cpp \
+     -fvisibility=hidden \
+     -fvisibility-inlines-hidden \
+     ../../src/LinkAPI.cpp \
        -lstdc++ \
        -m64 \
-       -I/usr/include/c++/4.4/i686-linux-gnu
+#       -I/usr/include/c++/4.4/i686-linux-gnu
 
 
 # for inspection of the library exports       
 # 32 bit
-nm -C -D libLinkAPI.so
+nm -C -D build/libLinkAPI.so
 # 64 bit
-nm -C -D libLinkAPI64.so
+nm -C -D build/libLinkAPI64.so
--- ide/scripts/make_osx.sh
+++ ide/scripts/make_osx.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-# UNTESTED!!
 
 # 32 bit
-rm -R build
+#rm -R build
 mkdir build
 
 g++ -dynamiclib -Wl,-headerpad_max_install_names,-undefined,dynamic_lookup,-compatibility_version,1.0,-current_version,1.0,-install_name,/usr/local/lib/libLinkAPI.dylib \
     -o build/libLinkAPI.dylib \
     -m32 \
-    ../../LinkAPI.cpp 
+    ../../src/LinkAPI.cpp 
 
 
 
---------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------
 
 
 # 64 bit
@@ -21,7 +20,7 @@ g++ -dynamiclib -Wl,-headerpad_max_install_names,-undefined,dynamic_lookup,-comp
 g++ -dynamiclib -Wl,-headerpad_max_install_names,-undefined,dynamic_lookup,-compatibility_version,1.0,-current_version,1.0,-install_name,/usr/local/lib/libLinkAPI64.dylib \
     -o build/libLinkAPI64.dylib \
     -m64 \
-    ../../LinkAPI.cpp 
+    ../../src/LinkAPI.cpp 
 
 
 
